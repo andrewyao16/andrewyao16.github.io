12a09f1fdd2544413ff178bceed0a7f6ac4e4337
andrewyao16 <78562095+andrewyao16@users.noreply.github.com>
2026-04-15 04:17:34 +0000

    Deploying to gh-pages from @ andrewyao16/andrewyao16.github.io@d3aeb4bc0192fb0b50cffcf2ae4316d29090d99a 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -419,9 +419,9 @@ ninja.data = [{
           description: "Aerosol mixing state effects on optical properties",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
+            },},{id: "projects-aerosol-methane-interaction",
+          title: 'Aerosol-methane interaction',
+          description: "Iron-catalyzed chlorine chemistry and methane budget",
           section: "Projects",handler: () => {
               window.location.href = "/projects/3_project/";
             },},{id: "projects-project-4",
